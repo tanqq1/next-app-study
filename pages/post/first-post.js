3bdd71ee0668ab954a21fefc87914ec340539564
tanqq <tanqingqing@qtarde.com.cn>
2023-06-19 19:00:35 +0800

    feat: next-app init

---
+++ pages/post/first-post.js
@@ -0,0 +1,16 @@
+import style from "../../styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+// import Script from "next/script";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+    </Layout>
+  );
+}
